d1d7b88ecd6fc83297878cb316f0afefd1aa1cd9
mkinney <mkinney@responsegroupinc.com>
2024-02-22 11:33:21 -0600

    Auto-commit changes #d6a3678

--- setup.py
+++ setup.py
@@ -24,19 +24,6 @@ else:
     __version__ = "unknown"
     print("Failed to retrieve the latest version from CHANGELOG.md")
 
-try:
-    version_line = open(version_fn, "rt").read()
-except EnvironmentError:
-    pass  # no version file
-else:
-    version_regex = r"^__version__ = ['\"]([^'\"]*)['\"]"
-    m = re.search(version_regex, version_line, re.M)
-    if m:
-        __version__ = m.group(1)
-    else:
-        print(f'unable to find version in {version_fn}')
-        raise RuntimeError(f'If {version_fn} exists, it is required to be well-formed')
-
 with open("README.md", "r") as rm:
     long_description = rm.read()
 
